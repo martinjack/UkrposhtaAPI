963787b31f049a81eba521a443e88873ce8eddf2
Jack Martin <killfess@gmail.com>
2017-10-07 16:00:27 +0300

    1

---
+++ README.md
@@ -0,0 +1,82 @@
+# Описание 
+
+PHP класс для работы с API Укрпочты
+
+# Документация
+
+[Google Disk](https://drive.google.com/file/d/0B-n0UjF7kxV_T253YU5nOHdCQlk/view?usp=sharing)
+
+# Требование
+
+* PHP не ниже 5.6
+* Composer
+
+# Методы API
+
+1. Создание адреса
+	* createAddress
+2. Редактирование адреса
+	* editAddress
+3. Показать адрес по ID
+	* getAddress
+4. Создание клиента
+	* createClient
+
+# Composer
+```bash
+composer require jackmartin/ukrpochta
+```
+# Примеры
+
+- createAddress($data = array())
+```php
+<?php
+use Ukrpochta\Pochta;
+
+include __DIR__.'/vendor/autoload.php';
+$ukrpochta = new Pochta('API_KEY');
+
+$result = $ukrpochta->createAddress(
+	array(
+		'postcode'			=>	'02099',
+		'region'			=>	'Полтавська',
+		'district'			=>	'Полтавський',
+		'city'				=>	'Полтава',
+		'street'			=>	'Шевченка',
+		'houseNumber' 		=>	'51',
+		'apartmentNumber'	=>	'20'
+	)
+);
+print_r($result);
+//{"id":123130,"postcode":"02099","region":"Полтавська","district":"Полтавський","city":"Полтава","street":"Шевченка",
+"houseNumber":"51","apartmentNumber":"20","description":null,"countryside":false,
+"detailedInfo":"Україна, 02099, Полтавська, Полтавський, Полтава, Шевченка, 51, 20","country":"UA"}
+```
+- editAddress($id, $data = array())
+```php
+<?php
+use Ukrpochta\Pochta;
+
+include __DIR__.'/vendor/autoload.php';
+$ukrpochta = new Pochta('API_KEY');
+
+$result = $ukrpochta->editAddress(123130,
+	array(
+		'postcode'			=>	'02099',
+		'region'			=>	'Полтавська',
+		'district'			=>	'Полтавський',
+		'city'				=>	'Полтава',
+		'street'			=>	'Шевченка',
+		'houseNumber' 		=>	'51',
+		'apartmentNumber'	=>	'20'
+	)
+);
+print_r($result);
+//{"id":123130,"postcode":"02099","region":"Полтавська","district":"Полтавський","city":"Полтава","street":"Шевченка",
+"houseNumber":"51","apartmentNumber":"20","description":null,"countryside":false,
+"detailedInfo":"Україна, 02099, Полтавська, Полтавський, Полтава, Шевченка, 51, 20","country":"UA"}
+```
+
+# Библиотеки 
+
+1. [Guzzle](https://github.com/guzzle/guzzle)
---
+++ src/Pochta.php
@@ -0,0 +1,236 @@
+<?php
+/**
+ *	module: Ukrpochta 0.0.1
+ *	author: Evgen Kitonin
+ *	version: 1
+ *	create: 06.10.2017
+ **/
+namespace Ukrpochta;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
+
+class Pochta
+{
+	/**
+	*	VERSION API
+	*	
+	*	@var string
+	*	
+	**/
+	private $version 	=	'0.0.1';
+	/**
+	*	API LINK
+	*	
+	*	@var string
+	*
+	**/
+	private $api 		=	'https://www.ukrposhta.ua/ecom/';
+	/**
+	*	API KEY
+	*
+	*	@var string
+	*	
+	**/
+	private $key 		=	null;
+	/**
+	*	INIT CLASS
+	*
+	*	@var function
+	*
+	**/
+	public function __construct($key)
+	{
+		return $this->setKey($key);
+	}
+	public function __destruct()
+	{
+	}
+	/**
+	*	
+	*	@param SET KEY
+	*	@return $this;
+	*
+	**/
+	public function setKey($key)
+	{
+		$this->key = $key;
+		return $this;
+	}
+	/**
+	*	CREATE LINK API
+	*
+	*	@param 	string 	$method 	METHOD REQUEST
+	*	@param 	string 	$param 		PARAMETERS
+	* 
+	*	@return $this->api
+	*	
+	**/
+	private function createLink($method, $param = '')
+	{
+		if ($param != '')
+			$param = '/' . $param; 
+
+		$this->api = $this->api . $this->version . '/' . $method . $param;
+	}
+	/**
+	*	PREPARE DATA
+	*
+	*	@param 	array 	$data 	ARRAY DATA
+	*
+	*	@return JSON
+	*
+	**/
+	private function prepare($data)
+	{
+		return json_encode($data, JSON_UNESCAPED_UNICODE);
+	}
+	/**
+	*	REQUEST DATA
+	*
+	*	@param 	string 	$method 	METHOD REQUEST
+	*	@param 	array 	$data 		ARRAY DATA
+	*	@param 	string 	$param 		PARAMETERS
+	*	@param 	string 	$type 		TYPE REQUEST
+	*
+	*	@return data
+	*	
+	**/
+	private function requestData($method, $data = '', $param = '', $type = 'post')
+	{
+		
+		$this->createLink($method, $param);
+
+		$client = new Client([
+		    'headers' => 
+			    [ 
+			    	'Content-Type' 	=>	'application/json',
+			    	'Authorization'	=>	'Bearer ' . $this->key 
+			    ]
+		]);
+
+		try {
+
+			switch($type) {
+
+				case 'post':
+					$response = $client->post($this->api, ['body'	=>	$this->prepare($data)]);
+				break;
+				case 'get':
+					$response = $client->get($this->api);
+				break;
+
+			}
+
+			return $response->getBody()->getContents();
+
+		} catch (RequestException $e) {
+
+			return $e->getResponse()->getBody()->getContents();
+
+		}
+	}
+	/**
+	*	СТВОРЕННЯ АДРЕСИ
+	*	СОЗДАНИЕ АДРЕСА
+	*
+	*	@param array 	$data 	ARRAY DATA
+	*
+	*	@return string
+	**/
+	public function createAddress($data = array())
+	{
+		return $this->requestData('addresses', $data);
+	}
+	/**
+	*	РЕДАГУВАННЯ АДРЕСИ
+	*	РЕДАКТИРОВАНИЕ АДРЕСА
+	*	
+	*	@param array 	$data 	ARRAY DATA
+	*	@param int 		$id 	ID ADDRESS
+	*
+	*	@return string
+	**/
+	public function editAddress($id, $data = array())
+	{
+		return $this->requestData('addresses', $data, $id);
+	}
+	/**
+	*	ПОКАЗАТЬ АДРЕС ПО ID
+	*	ПОКАЗАТЬ АДРЕС ПО ID
+	*	
+	* 	@param int 	$id 	ID ADDRESS
+	*
+	*	@return string
+	*
+	**/
+	public function getAddress($id)
+	{
+		return $this->requestData('addresses', '', $id, 'get');
+	}
+	/**
+	*	СТВОРЕННЯ КЛІЄНТА
+	*	СОЗДАНИЕ КЛИЕНТА
+	*	
+	*	@param string 	$token 	TOKEN COUNTERPARTY
+	*	@param array 	$data 	ARRAY DATA
+	*
+	*	@return string
+	*
+	**/
+	public function createClient($token, $data = array())
+	{
+		return $this->requestData('clients?token=' . $token, $data);
+	}
+	/**
+	*	РЕДАГУВАННЯ КЛІЄНТУ
+	*	РЕДАКТИРОВАНИЕ КЛИЕНТА
+	*
+	*	@param 	int 	$id 	ID CLIENT
+	*	@param 	string 	$token 	TOKEN COUNTERPARTY
+	*	@param 	array 	$array 	ARRAY DATA
+	*
+	*	@return string
+	*
+	**/
+	public function changeClient($id, $token, $data = array())
+	{
+		return $this->requestData('clients', $data, $id . '/?token=' . $token);
+	}
+	/**
+	*	ОТРИМАТИ СПИСОК КЛІЄНТІВ
+	*	ПОЛУЧИТЬ СПИСОК КЛИЕНТОВ
+	*
+	*	@param 	string 	$token 	TOKEN COUNTERPARTY
+	*
+	*	@return string
+	*
+	**/
+	public function clientsList($token)
+	{
+		return $this->requestData('clients/token=' . $token);
+	}
+	/**
+	*	ЗНАЙТИ КЛІЄНТА ПО ID
+	*	НАЙТИ КЛИЕНТА ПО ID
+	*
+	*	@param 	boolean 	$type 	TYPE REQUEST 	CLIENT ID || EXTERNAL ID
+	*	@param 	string 		$token 	TOKEN COUNTERPARTY
+	*	@param 	int 		$extID 	EXTERNAL ID CLIENT
+	*
+	*	@return string
+	*
+	**/
+	public function getClient($type = true, $token, $id = 0, $extID = 0)
+	{
+		if ($type) {
+
+			return $this->requestData('clients', '', $id . '/?token' . $token);
+
+		} else {
+
+			return $this->requestData('clients', '', '/external-id/' . $extID . '/?token=' . $token);
+
+		}
+	}
+}
