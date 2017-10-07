66efd7de128cd38f83c8da2b0ebafac7d771a941
Jack Martin <killfess@gmail.com>
2017-10-07 20:22:11 +0300

    fix

--- example.php
+++ example.php
@@ -4,7 +4,7 @@ use Ukrpochta\Pochta;
 
 include __DIR__ . '/vendor/autoload.php';
 
-$ukrpochta = new Pochta('f9027fbb-cf33-3e11-84bb-5484491e2c94');
+$ukrpochta = new Pochta('API_KEY');
 
 $result = $ukrpochta->createAddress(
     array(
@@ -18,7 +18,7 @@ $result = $ukrpochta->createAddress(
     )
 );
 
-print_r($result);
+// print_r($result);
 //{"id":123175,"postcode":"02099","region":"Полтавська","district":"Полтавський","city":"Полтава",
 //"street":"Шевченка","houseNumber":"51","apartmentNumber":"20","description":null,"countryside":false,
 //"detailedInfo":"Україна, 02099, Полтавська, Полтавський, Полтава, Шевченка, 51, 20","country":"UA"}
